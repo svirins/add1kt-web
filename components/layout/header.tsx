314e88495a4d988b2c937fa88903c45338f41757
Dzmitry Sviryn <zvirinz@outlook.com>
2022-05-25 21:40:02 +0300

    fix: remove mobile menu

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from 'react';
 
 import LocaleSwitcher from '@/components/misc/locale-switcher';
 import ThemeSwitcher from '@/components/misc/theme-switcher';
-import MobileMenu from '@/components/misc/mobile-menu';
 import NavItemHeader from '@/components/misc/nav-item-header';
 import Config from '@/config/global-config';
 
@@ -17,12 +16,12 @@ function Header() {
   const router = useRouter();
   return (
     <div className="flex flex-col justify-center px-8">
-      <nav className="flex items-center justify-between w-full relative max-w-3xl  mx-auto pt-8 pb-8 md:pb-16">
+      <nav className="flex items-center justify-between w-full relative max-w-3xl  mx-auto pt-4 pb-4 md:pb-8">
         <a href="#skip" className="skip-nav">
           Skip to content
         </a>
         <div className="ml-[-0.60rem] inline-flex items-center">
-          <MobileMenu />
+          {/* <MobileMenu /> */}
           {Config.menuLinks.map((link, index) => (
             <NavItemHeader
               href={link.href}
