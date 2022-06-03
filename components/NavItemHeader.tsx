7c5c1f4824b6cc6cc999e4a44105d4825a8eb8a7
Dzmitry Sviryn <zvirinz@outlook.com>
2022-06-03 16:43:57 +0300

    chore: minor styling

--- components/NavItemHeader.tsx
+++ components/NavItemHeader.tsx
@@ -11,7 +11,7 @@ function NavItemHeader({ href, text }) {
       <a
         className={cn(
           isActive ? ' text-teal-600' : '',
-          'font-medium text-base md:text-xl  inline-block p-1 lg:py-4 items-center transition-all delay-100 hover:text-teal-800 dark:hover:text-teal-400'
+          'font-medium text-base md:text-xl  inline-block p-2 lg:py-4 items-center transition-all delay-100 hover:text-teal-800 dark:hover:text-teal-400'
         )}
       >
         <span className="capsize">{text}</span>
