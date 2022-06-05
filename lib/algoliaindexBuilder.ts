0774efd64fa71eb197cc112189deb422e9a243cc
Dzmitry Sviryn <zvirinz@outlook.com>
2022-06-05 21:14:26 +0300

    refactor: api

---
+++ lib/algoliaindexBuilder.ts
@@ -0,0 +1,53 @@
+import algoliasearch from 'algoliasearch';
+import { createClient } from 'next-sanity';
+import { groq } from 'next-sanity';
+
+import { localizedAlgoliaIndices } from '@/lib/config';
+
+const sanityConfig = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: '2021-03-25',
+  useCdn: false
+};
+
+const client = createClient(sanityConfig);
+
+const algoliaInstance = algoliasearch(
+  process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
+  process.env.ALGOLIA_SEARCH_ADMIN_KEY
+);
+
+const getTotalPostForAlgoliaQuery = groq`*[_type == 'post'] {
+  "objectID": _id,
+  "title": title[$locale],
+  "slug": slug.current,
+  "tags": tags[] -> title[$locale]
+}`;
+
+const generateIndexPerLocale = async (indexName, locale) => {
+  const data = await client.fetch(getTotalPostForAlgoliaQuery, { locale });
+  const index = algoliaInstance.initIndex(indexName);
+
+  try {
+    console.time(`Saving ${data.length} documents to index: ${indexName}`);
+    await index.saveObjects(data);
+    console.timeEnd(`Saving ${data.length} documents to index:  ${indexName}`);
+    return {
+      status: 200,
+      body: 'Success!'
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      status: 500,
+      body: error
+    };
+  }
+};
+
+export default async function generateAlgoliaIndex() {
+  for await (const i of localizedAlgoliaIndices) {
+    generateIndexPerLocale(i.indexName, i.locale);
+  }
+}
--- pages/api/algolia/index.ts
+++ pages/api/algolia/index.ts
@@ -1,60 +1,12 @@
-import algoliasearch from 'algoliasearch';
-import { createClient } from 'next-sanity';
-import { groq } from 'next-sanity';
-
-import { localizedAlgoliaIndices } from '@/lib/config';
-
-const sanityConfig = {
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  apiVersion: '2021-03-25',
-  useCdn: false
-};
-
-const client = createClient(sanityConfig);
-
-const algoliaInstance = algoliasearch(
-  process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
-  process.env.ALGOLIA_SEARCH_ADMIN_KEY
-);
-
-const getTotalPostForAlgoliaQuery = groq`*[_type == 'post'] {
-  "objectID": _id,
-  "title": title[$locale],
-  "slug": slug.current,
-  "tags": tags[] -> title[$locale]
-}`;
-
-const generateIndexPerLocale = async (indexName, locale) => {
-  const data = await client.fetch(getTotalPostForAlgoliaQuery, { locale });
-  const index = algoliaInstance.initIndex(indexName);
-
-  try {
-    console.time(`Saving ${data.length} documents to index: ${indexName}`);
-    await index.saveObjects(data);
-    console.timeEnd(`Saving ${data.length} documents to index:  ${indexName}`);
-    return {
-      status: 200,
-      body: 'Success!'
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      status: 500,
-      body: error
-    };
-  }
-};
-
-async function generate() {
-  for await (const i of localizedAlgoliaIndices) {
-    generateIndexPerLocale(i.indexName, i.locale);
-  }
-}
+import { NextApiRequest, NextApiResponse } from "next";
+import generateAlgoliaIndex from "@/lib/algoliaindexBuilder";
 
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // security check
+
+  // fire generator function
   res.status(200).json({ name: 'John Doe' });
-  generate()
+  generateAlgoliaIndex();
   console.log('webhook fired from Sanity');
 }
