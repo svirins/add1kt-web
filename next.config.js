27c38ecf2976a2a7be48aab4016448508528fd8b
Dzmitry Sviryn <zvirinz@outlook.com>
2022-06-29 20:07:04 +0300

    refactor: remove preact

--- next.config.js
+++ next.config.js
@@ -36,16 +36,4 @@ module.exports = withBundleAnalyzer({
   async rewrites() {
     return [BEE_REWRITE, HIVE_REWRITE];
   },
-  // webpack: (config, { dev, isServer }) => {
-  //   // Replace React with Preact only in client production build
-  //   if (!dev && !isServer) {
-  //     Object.assign(config.resolve.alias, {
-  //       react: "preact/compat",
-  //       "react-dom/test-utils": "preact/test-utils",
-  //       "react-dom": "preact/compat",
-  //     });
-  //   }
-
-  //   return config;
-  // },
 });
